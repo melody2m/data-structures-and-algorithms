7019c16e6d8e07ddeaf197a68192df91130e6433
melody2m <melody2musik@gmail.com>
2018-05-30 23:16:51 -0700

    tree-intersection

---
+++ tree-intersection/tree-intersection.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const findMax = module.exports = {};
+
+let 
+
+
+max = 0;
+
+findMax.traversal = (rootNode) => {
+  if (!rootNode) {
+    return null;
+  }
+  if (rootNode.value > max) {
+    max = rootNode.value;
+  }
+  console.log(max);
+
+  findMax.traversal(rootNode.left);
+  findMax.traversal(rootNode.right);
+  return max;
+};
